e5ac059bfc9726d2e9c8d00476709d45cfe5a57a
Shiloh Alleyne <83500298+ShilohAlleyne@users.noreply.github.com>
2021-05-03 22:22:41 +0100

    Update Proteome Analysis Terminal.py

--- Proteome Analysis Terminal.py
+++ Proteome Analysis Terminal.py
@@ -6,6 +6,8 @@ os.chdir(r'C:\Users\pc\Documents\Uni\Level 6\Final Project\Bacterial Efflux Pump
 with open('Type 1 Motifs.txt') as b:
     Type_1 = b.read()
 
+# NOTE: Example of a called function. Here the function for searching for Type 2 motifs
+# NOTE: is being used to search Type 1 containg proteins for said Type 2 motif
 Type_2_Search.VEKSSSSF(Type_1)
 Type_2_Search.SSSS(Type_1)
 Type_2_Search.EKSSSS(Type_1)
